7e41422a3b9fe3b236e62e92ea41de3178fe4caf
Bianca Castanheira <biancacastanheira@live.com>
2021-06-15 21:53:22 -0300

    ✨ Initial page setup

---
+++ src/pages/PurchaseConfirmation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PurchaseConfirmation = () => {
+  return (
+    <div>
+      <h1>Purchase Confirmation</h1>
+    </div>
+  );
+};
+
+export default PurchaseConfirmation;
---
+++ src/pages/SubscriptionPlans.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const SubscriptionPlans = () => {
+  return (
+    <div>
+      <Typography variant="h3">Confira o seu plano:</Typography>
+    </div>
+  );
+};
+
+export default SubscriptionPlans;
