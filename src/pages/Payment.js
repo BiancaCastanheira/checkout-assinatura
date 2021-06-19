ce25d4bb7d33aa4e4dc25ad2dd6e73edd454bc4f
Bianca Castanheira <biancacastanheira@live.com>
2021-06-19 17:28:15 -0300

    ✨ Add mask in the form.

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -7,11 +7,17 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
+import {
+  maskCreditCardNumber,
+  maskExpirationDate,
+  maskCvv,
+  maskCpf,
+} from "../utils/InputMasker";
 import { postClientCardInfo } from "../api/paymentApi";
 import ButtonCheckout from "../components/ButtonCheckout";
 
 const Payment = () => {
-  const [cardNumer, setCardNumber] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
   const [expirationDate, setExpirationDate] = useState("");
   const [cvv, setCvv] = useState("");
   const [cardHolder, setCardHolder] = useState("");
@@ -27,20 +33,30 @@ const Payment = () => {
       creditCardCVV: cvv,
       credifCardExpirationDate: expirationDate,
       creditCardHolder: cardHolder,
-      creditCardNumber: cardNumer,
+      creditCardNumber: cardNumber,
       gateway: localStorage.getItem("gateway"),
       installments: localStorage.getItem("installments"),
       offerId: localStorage.getItem("offerId"),
       userId: localStorage.getItem("userId"),
     };
-    postClientCardInfo(clientCard).then((respose) => {
-      console.log(respose);
-    });
+    postClientCardInfo(clientCard)
+      .then((respose) => {
+        console.log(respose);
+      })
+      .catch((error) => {
+        console.error(
+          "Someting went wrong when posting the client's card information."
+        );
+      });
   };
 
   let menuItemsInstallments = [];
   for (let i = 1; i <= possibleInstallments; i++) {
-    menuItemsInstallments.push(<MenuItem value={i}>{i}</MenuItem>);
+    menuItemsInstallments.push(
+      <MenuItem value={i} key={i}>
+        {i}
+      </MenuItem>
+    );
   }
 
   return (
@@ -51,27 +67,33 @@ const Payment = () => {
       </Typography>
 
       <TextField
-        id="card-number"
+        id="cartao"
         label="Número do cartão"
+        placeholder="0000 0000 0000 0000"
         fullWidth={true}
-        value={cardNumer}
-        onChange={(e) => setCardNumber(e.target.value)}
+        value={cardNumber}
+        onChange={(e) => setCardNumber(maskCreditCardNumber(e.target.value))}
       />
+
       <Grid container spacing={1}>
         <Grid item xs>
           <TextField
             id="expiration-date"
             label="Validade"
+            placeholder="MM/AA"
             value={expirationDate}
-            onChange={(e) => setExpirationDate(e.target.value)}
+            onChange={(e) =>
+              setExpirationDate(maskExpirationDate(e.target.value))
+            }
           />
         </Grid>
         <Grid item xs>
           <TextField
             id="cvv"
             label="CVV"
+            placeholder="000"
             value={cvv}
-            onChange={(e) => setCvv(e.target.value)}
+            onChange={(e) => setCvv(maskCvv(e.target.value))}
           />
         </Grid>
       </Grid>
@@ -80,6 +102,7 @@ const Payment = () => {
         <TextField
           id="card-holder"
           label="Nome impresso no cartão"
+          placeholder="Seu nome"
           fullWidth={true}
           value={cardHolder}
           onChange={(e) => setCardHolder(e.target.value)}
@@ -90,9 +113,10 @@ const Payment = () => {
         <TextField
           id="cpf"
           label="CPF"
+          placeholder="000.000.000-00"
           fullWidth={true}
           value={cpf}
-          onChange={(e) => setCpf(e.target.value)}
+          onChange={(e) => setCpf(maskCpf(e.target.value))}
         />
       </Grid>
 
@@ -100,6 +124,7 @@ const Payment = () => {
         <TextField
           id="coupon"
           label="Cupom"
+          placeholder="Insira aqui"
           fullWidth={true}
           value={coupon}
           onChange={(e) => setCoupon(e.target.value)}
@@ -112,6 +137,7 @@ const Payment = () => {
           <Select
             labelId="installments"
             id="installments"
+            placeholder="Selecionar"
             value={installments}
             onChange={(e) => setInstallments(e.target.value)}
           >
